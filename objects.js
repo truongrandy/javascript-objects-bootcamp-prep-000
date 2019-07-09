2b59d63cc6ca9eb553a60fee2e5820904e5b1e79
Randy Truong <truongrandy@gmail.com>
2019-07-09 20:44:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,16 +3,16 @@ var meals = {
   lunch: "turkey sandwich",
   dinner: "steak and potatoes"
 };
- 
- 
+
+
 Object.assign({}, meals, { breakfast: ['oatmeal', 'banana'] })
 // returns {
 //   breakfast: ["oatmeal", "banana"],
 //   lunch: "turkey sandwich",
 //   dinner: "steak and potatoes"
 //  }
- 
- 
+
+
 meals
 // still {
 //   breakfast: "oatmeal",
